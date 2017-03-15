b671719fb77a172e53c4fc1b0e954134cc64efef
Kevin Yang <yangkevi@usc.edu>
2017-03-15 01:19:21 -0700

    sad broken recyclerview

--- app/src/main/java/com/kevinkyang/inventory/InventoryFragment.java
+++ app/src/main/java/com/kevinkyang/inventory/InventoryFragment.java
@@ -1,8 +1,11 @@
 package com.kevinkyang.inventory;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
@@ -22,8 +25,12 @@ public class InventoryFragment extends Fragment implements CustomFragment {
 	private MainActivity parent;
 
 	private ItemData itemData = null;
-	private ListView inventoryListView;
-	private ItemAdapter itemAdapter;
+//	private ListView inventoryListView; TODO
+//	private ItemAdapter itemAdapter; TODO
+
+	private RecyclerView itemRecyclerView;
+	private ItemRVAdapter itemRVAdapter;
+	private RecyclerView.LayoutManager layoutManager;
 
 	private String inventory = null;
 	private boolean initFinished = false;
@@ -32,20 +39,28 @@ public class InventoryFragment extends Fragment implements CustomFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_inventory, container, false);
-		inventoryListView = (ListView) view.findViewById(R.id.inventory_listview);
+//		inventoryListView = (ListView) view.findViewById(R.id.inventory_listview); TODO
+		itemRecyclerView = (RecyclerView) view.findViewById(R.id.inventory_rv);
 		return view;
 	}
 
 	@Override
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-		// this fragment can only be attached to MainActivity
+		// TODO this fragment can only be attached to com.kevinkyang.inventory.MainActivity, is this safe?
 		parent = (MainActivity) getActivity();
 
 		itemData = ItemData.getInstance();
-		itemAdapter = new ItemAdapter(parent,
-				new ArrayList<Item>(), this);
-		inventoryListView.setAdapter(itemAdapter);
-		registerForContextMenu(inventoryListView);
+//		itemAdapter = new ItemAdapter(parent,
+//				new ArrayList<Item>(), this); TODO
+//		inventoryListView.setAdapter(itemAdapter); TODO
+//		registerForContextMenu(inventoryListView); TODO
+
+		itemRVAdapter = new ItemRVAdapter(new ArrayList<Item>(), this);
+		itemRecyclerView.setAdapter(itemRVAdapter);
+		layoutManager = new LinearLayoutManager(parent);
+		itemRecyclerView.setLayoutManager(layoutManager);
+		itemRecyclerView.setHasFixedSize(false); // TODO enable this when you restrict item height later, right now long titles elongate an item
+		registerForContextMenu(itemRecyclerView);
 
 		initFinished = true;
 
@@ -59,7 +74,7 @@ public class InventoryFragment extends Fragment implements CustomFragment {
 
 		showInventory(inventory);
 
-		addListeners();
+//		addListeners(); TODO
 		super.onActivityCreated(savedInstanceState);
 	}
 
@@ -82,21 +97,23 @@ public class InventoryFragment extends Fragment implements CustomFragment {
 
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
-		AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-		Item it = itemAdapter.getItem(menuInfo.position);
-		switch (item.getItemId()) {
-			case R.id.list_item_delete:
-				if (itemData.removeItem(it)) {
-					refresh();
-					return true;
-				}
-				else return false;
-			case R.id.list_item_add_to_grocery:
-				swapToGroceryList(it);
-				return true;
-			default:
-				return super.onContextItemSelected(item);
-		}
+		//TODO need to implement context menu for recyclerview
+//		AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+//		Item it = itemAdapter.getItem(menuInfo.position);
+//		switch (item.getItemId()) {
+//			case R.id.list_item_delete:
+//				if (itemData.removeItem(it)) {
+//					refresh();
+//					return true;
+//				}
+//				else return false;
+//			case R.id.list_item_add_to_grocery:
+//				swapToGroceryList(it);
+//				return true;
+//			default:
+//				return super.onContextItemSelected(item);
+//		}
+		return super.onContextItemSelected(item); //TODO
 	}
 
 	/**
@@ -116,16 +133,22 @@ public class InventoryFragment extends Fragment implements CustomFragment {
 		return inventory;
 	}
 
+	public MainActivity getParent() {
+		return parent;
+	}
+
 	/**
 	 * Call this when changes occur in other parts of the
 	 * app that affect the inventory list.
 	 */
 	@Override
 	public void refresh() {
-		itemAdapter.notifyDataSetInvalidated();
-		itemAdapter = new ItemAdapter(parent,
-				itemData.getItemsByInventory(inventory), this);
-		inventoryListView.setAdapter(itemAdapter);
+		itemRVAdapter.setItemsList(
+				itemData.getItemsByInventory(inventory));
+//		itemAdapter.notifyDataSetInvalidated(); TODO
+//		itemAdapter = new ItemAdapter(parent,
+//				itemData.getItemsByInventory(inventory), this);
+//		inventoryListView.setAdapter(itemAdapter);
 	}
 
 	public void setInventory(String inventory) {
@@ -136,15 +159,15 @@ public class InventoryFragment extends Fragment implements CustomFragment {
 		return initFinished;
 	}
 
-	private void addListeners() {
-		inventoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Item item = (Item) inventoryListView.getItemAtPosition(position);
-				InventoryFragment.this.parent.showEditDialog(item);
-			}
-		});
-	}
+//	private void addListeners() { TODO
+//		inventoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//			@Override
+//			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//				Item item = (Item) inventoryListView.getItemAtPosition(position);
+//				InventoryFragment.this.parent.showEditDialog(item);
+//			}
+//		});
+//	}
 
 	private void swapToGroceryList(Item item) {
 		//TODO might be able to do this by calling DbManager.updateItemColumn
--- app/src/main/java/com/kevinkyang/inventory/ItemRVAdapter.java
+++ app/src/main/java/com/kevinkyang/inventory/ItemRVAdapter.java
@@ -1,8 +1,12 @@
 package com.kevinkyang.inventory;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -11,43 +15,169 @@ import java.util.ArrayList;
  * Created by Kevin on 3/13/2017.
  */
 
-public class ItemRVAdapter extends RecyclerView.Adapter<ItemRVAdapter.ViewHolder> {
+public class ItemRVAdapter
+		extends RecyclerView.Adapter<ItemRVAdapter.ViewHolder> {
 	private ArrayList<Item> items;
+	private InventoryFragment parent;
+	private DBManager dbManager;
+	private int defaultColor;
 
-	public static class ViewHolder extends RecyclerView.ViewHolder {
+	public static class ViewHolder extends RecyclerView.ViewHolder
+			implements View.OnClickListener {
+		public View itemView;
+
 		public TextView name;
-		public String createdDate;
-		public String expiresDate;
-		public int quantity;
-		public String unit;
-		public String type;
-		public String inventory;
-		public boolean inGroceryList;
+		public LinearLayout expiresContainer;
+		public TextView expiresNum;
+		public TextView expiresUnit;
+		public TextView quantity;
+		public TextView quantityUnit;
+		public ImageButton decQuantityButton;
+		public ImageButton incQuantityButton;
 
-		public int daysUntilExpiration;
+		public ViewHolderClickListener listener;
 
-		public ViewHolder(View itemView, TextView name, String createdDate, String expiresDate, int quantity, String unit, String type, String inventory, boolean inGroceryList, int daysUntilExpiration) {
+		public ViewHolder(View itemView, TextView name,
+						  LinearLayout expiresContainer,
+						  TextView expiresNum, TextView expiresUnit,
+						  TextView quantity, TextView quantityUnit,
+						  ImageButton decQuantityButton,
+						  ImageButton incQuantityButton,
+						  ViewHolderClickListener listener) {
 			super(itemView);
+			this.itemView = itemView;
 			this.name = name;
-			this.createdDate = createdDate;
-			this.expiresDate = expiresDate;
+			this.expiresContainer = expiresContainer;
+			this.expiresNum = expiresNum;
+			this.expiresUnit = expiresUnit;
 			this.quantity = quantity;
-			this.unit = unit;
-			this.type = type;
-			this.inventory = inventory;
-			this.inGroceryList = inGroceryList;
-			this.daysUntilExpiration = daysUntilExpiration;
+			this.quantityUnit = quantityUnit;
+			this.decQuantityButton = decQuantityButton;
+			this.incQuantityButton = incQuantityButton;
+			this.listener = listener;
 		}
+
+		@Override
+		public void onClick(View v) {
+			listener.onClick(getAdapterPosition());
+		}
+
+		public static interface ViewHolderClickListener {
+			public void onClick(int position);
+		}
+	}
+
+	public ItemRVAdapter(ArrayList<Item> items, InventoryFragment parent) {
+		this.items = items;
+		this.parent = parent;
+		dbManager = DBManager.getInstance();
+		defaultColor = -1;
+
+		setHasStableIds(true);
 	}
 
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		return null;
+		View view = LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.item, parent, false);
+
+		TextView name = (TextView) view.findViewById(R.id.item_name);
+		LinearLayout expiresContainer = (LinearLayout) view.findViewById(R.id.expires_container);
+		TextView expiresNum = (TextView) view.findViewById(R.id.expires_num);
+		TextView expiresUnit = (TextView) view.findViewById(R.id.expires_unit);
+		TextView quantity = (TextView) view.findViewById(R.id.quantity);
+		TextView quantityUnit = (TextView) view.findViewById(R.id.quantity_unit);
+		ImageButton decQuantityButton = (ImageButton) view.findViewById(R.id.decrease_quantity);
+		ImageButton incQuantityButton = (ImageButton) view.findViewById(R.id.increase_quantity);
+
+		ViewHolder.ViewHolderClickListener listener =
+				new ViewHolder.ViewHolderClickListener() {
+					@Override
+					public void onClick(int position) {
+						Item item = items.get(position);
+						ItemRVAdapter.this
+								.parent.getParent()
+								.showEditDialog(item);
+					}
+				};
+
+		ViewHolder holder =
+				new ViewHolder(view, name, expiresContainer, expiresNum,
+						expiresUnit, quantity, quantityUnit,
+						decQuantityButton, incQuantityButton,
+						listener);
+		return holder;
 	}
 
 	@Override
-	public void onBindViewHolder(ViewHolder holder, int position) {
+	public void onBindViewHolder(final ViewHolder holder, int position) {
+		Item item = items.get(position);
 
+		holder.name.setText(item.getName());
+
+		String expiresDate = item.getExpiresDate();
+		if (!expiresDate.isEmpty()) {
+			int dateDifference = TimeManager.getDateDifferenceInDays(
+					TimeManager.getDateTimeLocal(),
+					expiresDate);
+			String convertedTime = TimeManager.convertDays(dateDifference);
+			String[] splitString = convertedTime.split(" ");
+			holder.expiresNum.setText(splitString[0]);
+			holder.expiresUnit.setText(splitString[1]);
+			if (dateDifference < 0) {
+				int color = holder.itemView
+						.getContext()
+						.getResources()
+						.getColor(
+								android.R.color.holo_red_dark, null);
+				setExpirationColor(holder, color);
+			} else {
+				if (defaultColor == -1) {
+					Context context = holder.itemView.getContext();
+					defaultColor = context.getResources()
+							.getColor(R.color.defaultTextColor,
+									context.getTheme());
+				}
+				setExpirationColor(holder, defaultColor);
+			}
+			setExpirationVisibility(holder, View.VISIBLE);
+		} else {
+			setExpirationVisibility(holder, View.INVISIBLE);
+		}
+
+		holder.quantity.setText(Integer.toString(item.getQuantity()));
+
+		String unitString = item.getUnit().trim();
+		if (unitString.isEmpty()) {
+			holder.quantityUnit.setVisibility(View.GONE);
+		} else {
+			holder.quantityUnit.setText(item.getUnit());
+			holder.quantityUnit.setVisibility(View.VISIBLE);
+		}
+
+		View.OnClickListener quantityListener = new View.OnClickListener() {
+			public void onClick(View view) {
+				int amount = 0;
+				switch (view.getId()) {
+					case R.id.decrease_quantity: amount = -1; break;
+					case R.id.increase_quantity: amount = 1; break;
+					default: break;
+				}
+
+				Item item = items.get(holder.getAdapterPosition());
+				if (item != null) {
+					item.setQuantity(item.getQuantity() + amount);
+					dbManager.updateItemColumn(item, DBSchema.TABLE_ITEMS.COL_QUANTITY);
+					ItemRVAdapter.this.parent.refresh();
+				}
+			}
+		};
+
+		holder.decQuantityButton.setFocusable(false);
+		holder.decQuantityButton.setOnClickListener(quantityListener);
+
+		holder.incQuantityButton.setFocusable(false);
+		holder.incQuantityButton.setOnClickListener(quantityListener);
 	}
 
 	@Override
@@ -55,4 +185,23 @@ public class ItemRVAdapter extends RecyclerView.Adapter<ItemRVAdapter.ViewHolder
 		return items.size();
 	}
 
+	public void setItemsList(final ArrayList<Item> newItems) {
+		int oldSize = items.size();
+		items.clear();
+		notifyItemRangeRemoved(0, oldSize);
+		items.addAll(newItems);
+		notifyItemRangeInserted(0, items.size());
+	}
+
+	private void setExpirationVisibility(ViewHolder holder, int visibility) {
+		holder.expiresContainer.setVisibility(visibility);
+		holder.expiresNum.setVisibility(visibility);
+		holder.expiresUnit.setVisibility(visibility);
+	}
+
+	private void setExpirationColor(ViewHolder holder, int color) {
+		holder.name.setTextColor(color);
+		holder.expiresNum.setTextColor(color);
+		holder.expiresUnit.setTextColor(color);
+	}
 }
